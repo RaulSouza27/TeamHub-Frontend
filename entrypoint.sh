48e42a961742f38add244068065de56afe9cc55e
RaulSouza27 <rauljack25@gmail.com>
2026-05-25 22:17:09 -0300

    change entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-sed -i "s/\$PORT/${PORT:-80}/g" /etc/nginx/conf.d/teamhub.conf
+# Pega o PORT injetado pelo Railway (fallback 8080)
+PORTA="${PORT:-8080}"
+echo "Iniciando nginx na porta $PORTA"
+
+# Substitui a string literal '$PORT' no config
+sed -i "s/\$PORT/$PORTA/g" /etc/nginx/conf.d/teamhub.conf
+
+# Confirma o resultado (aparece nos logs do Railway)
+grep "listen" /etc/nginx/conf.d/teamhub.conf
+
 nginx -g "daemon off;"
